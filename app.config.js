b9173e30ffef1dd23bd91568389581bcf11dbba3
andromeda2268142-byte <andromeda2268142@gmail.com>
2026-09-20 23:12:50 -0400

    Use react-native-maps config plugin for Google keys

--- app.config.js
+++ app.config.js
@@ -3,29 +3,18 @@ const base = require('./app.json').expo;
 module.exports = () => {
   const iosMapsKey = process.env.EXPO_PUBLIC_GOOGLE_MAPS_IOS_KEY;
   const androidMapsKey = process.env.EXPO_PUBLIC_GOOGLE_MAPS_ANDROID_KEY;
+  const mapsOptions = {
+    ...(androidMapsKey ? { androidGoogleMapsApiKey: androidMapsKey } : {}),
+    ...(iosMapsKey ? { iosGoogleMapsApiKey: iosMapsKey } : {}),
+  };
 
   return {
     ...base,
-    ios: {
-      ...base.ios,
-      config: {
-        ...(base.ios?.config || {}),
-        ...(iosMapsKey ? { googleMapsApiKey: iosMapsKey } : {}),
-      },
-    },
-    android: {
-      ...base.android,
-      config: {
-        ...(base.android?.config || {}),
-        ...(androidMapsKey
-          ? {
-              googleMaps: {
-                ...(base.android?.config?.googleMaps || {}),
-                apiKey: androidMapsKey,
-              },
-            }
-          : {}),
-      },
-    },
+    plugins: [
+      ...(base.plugins || []),
+      ...(Object.keys(mapsOptions).length
+        ? [['react-native-maps', mapsOptions]]
+        : []),
+    ],
   };
 };
